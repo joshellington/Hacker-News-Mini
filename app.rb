3657d1f70c498c7703b44b7f98068bd5a6b5565e
Josh Ellington <hello@joshellington.com>
2011-09-05 18:11:15 -0700

    added ask/new sections, tweaked styling

--- app.rb
+++ app.rb
@@ -15,6 +15,8 @@ match '/' do
   @entries = Entry.all
   @next_page = 2
 
+  @title = 'Home'
+
   erb :home
 end
 
@@ -23,8 +25,24 @@ match '/page/:page/?' do
   @entries = Entry.all(page).to_a.reverse.shift(30)
   @entries.reverse!
 
+  @title = 'Page '+page
+
   @next_page = page + 1
   @prev_page = page - 1
+
+  erb :home
+end
+
+match '/ask/?' do
+  @entries = Entry.questions
+  @title = 'Ask'
+
+  erb :home
+end
+
+match '/new/?' do
+  @entries = Entry.newest
+  @title = 'New'
 
   erb :home
 end
